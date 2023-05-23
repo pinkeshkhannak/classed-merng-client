bb490d847b75e8830489b7a1e2f1fb3b7462e56f
pinkeshkhannak <pinkeshkhanna56@gmail.com>
2023-05-23 12:43:01 +0530

    fix_createHttp Link

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import {
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:10000/",
+  uri: "https://webapp-qapl.onrender.com/",
 });
 
 const authLink = setContext((_, { headers }) => {
